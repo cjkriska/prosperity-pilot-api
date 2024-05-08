7f5cf8c347a67d90232a3ab6f5e160bacfb51818
cjkriska <charleskriska@gmail.com>
2024-05-08 15:50:35 -0500

    initial commit

---
+++ src/main/java/com/charliekriska/prosperitypilotapi/ProsperityPilotApiApplication.java
@@ -0,0 +1,13 @@
+package com.charliekriska.prosperitypilotapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProsperityPilotApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProsperityPilotApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/charliekriska/prosperitypilotapi/ProsperityPilotApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.charliekriska.prosperitypilotapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProsperityPilotApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
